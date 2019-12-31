0c30932d2a90fe09364e299f4e901d97704f4cd7
Qoosim <ayinde.abdulghaniyy@gmail.com>
2019-12-31 20:57:36 +0100

    three functionalities tested on my_each_with_index method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require './lib/enumerables.rb'
+require './lib/enumerables'
 RSpec.describe Enumerable do
   let(:arr) { [] }
 
@@ -15,4 +15,17 @@ RSpec.describe Enumerable do
     end
   end
 
+  describe '#my_each_with_index' do
+    it 'returns elements numerical position' do
+      [2, 3, 5, 7].my_each_with_index { |_elem, idx| arr << idx }
+      expect(arr).to eql([0, 1, 2, 3])
+    end
+    it 'returns elements of an array' do
+      [2, 3, 5, 7].my_each_with_index { |elem, _idx| arr << elem}
+      expect(arr).to eql([2, 3, 5, 7])
+    end
+    it 'returns enumerator when no block is given' do
+      expect([2, 3, 5, 7].my_each_with_index).to be_a(Enumerator)
+    end
+  end
 end
