bab97f95b7481735e7a43a77dadf1f7b04dbed3c
Qoosim <ayinde.abdulghaniyy@gmail.com>
2019-12-31 21:14:30 +0100

    possible functionalities tested on my_all method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -21,11 +21,29 @@ RSpec.describe Enumerable do
       expect(arr).to eql([0, 1, 2, 3])
     end
     it 'returns elements of an array' do
-      [2, 3, 5, 7].my_each_with_index { |elem, _idx| arr << elem}
+      [2, 3, 5, 7].my_each_with_index { |elem, _idx| arr << elem }
       expect(arr).to eql([2, 3, 5, 7])
     end
     it 'returns enumerator when no block is given' do
       expect([2, 3, 5, 7].my_each_with_index).to be_a(Enumerator)
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns elements of an array that passed the block condition' do
+      expect([2, 'ant', 3, 'boat', 4, 'cat'].my_select { |item| item.class == String }).to eql(%w[ant boat cat])
+    end
+    it 'returns Enumerator when no block is given' do
+      expect([2, 'ant', 3, 'boat', 4, 'cat'].my_select).to be_a(Enumerator)
+    end
+  end
+
+  describe '#my_all?' do
+    it 'returns true if the block never returns false or nil' do
+      expect(%w[ant ball cat dog ram].my_all? { |item| item.size >= 3 }).to eql(true)
+    end
+    it 'returns Enumerator if no block is given' do
+      expect(%w[ant ball cat dog ram].my_all?).to eql(true)
     end
   end
 end
